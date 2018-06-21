b26c32703e0fc9c233d2d92d992f0f70c9c5633e
azeembootwala <azeem.bootwala@seppmed.de>
2018-06-21 12:07:09 +0200

    updated identity block

--- Layers/IdentityBlock.py
+++ Layers/IdentityBlock.py
@@ -47,7 +47,7 @@ class IdentityBlock:
         params = []
         for layer in self.layers:
             params+=layer.get_params()
-        return params
+        return paramsdd
 
 
 if __name__ == '__main__':
